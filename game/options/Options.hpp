19c2ba4b286f1664f8ae561b4702d43ddc0109eb
kirillirik <kirillirikvideo@gmail.com>
2024-05-13 13:20:20 +0300

    feat: default Options

--- game/options/Options.hpp
+++ game/options/Options.hpp
@@ -14,19 +14,27 @@ namespace Avox {
     class Options {
     public:
         static void loadOptions() {
+            defaultOptions();
+
             try {
                 toml::table table = toml::parse_file(R"(..\game\resources\options.toml)");
                 useConfiguration = table["use_configuration"].as_boolean()->get();
-                width = table["width"].as_integer()->get();
-                height = table["height"].as_integer()->get();
-                radius = table["radius"].as_integer()->get();
+
+                if (useConfiguration) {
+                    width = table["width"].as_integer()->get();
+                    height = table["height"].as_integer()->get();
+                    radius = table["radius"].as_integer()->get();
+                }
             } catch (const toml::parse_error& err) {
-                useConfiguration = false;
-                width = 2560;
-                height = 1440;
-                radius = 2;
                 std::cerr << "Parsing failed:\n" << err << "\n";
             }
+        }
+
+        static void defaultOptions() {
+            useConfiguration = false;
+            width = 2560;
+            height = 1440;
+            radius = 2;
         }
 
         static bool useConfiguration;
